097f5c0713efdf14a5840d16908068db552f1e4b
Thomás Sousa Silva (ThomasSousa96) <thomas.sousa.96@gmail.com>
2018-09-09 21:31:07 -0300

    Redesign the library to support multiple message sources.

--- src/main/java/com/infinityrefactoring/util/message/DefaultMessageInterpolator.java
+++ src/main/java/com/infinityrefactoring/util/message/DefaultMessageInterpolator.java
@@ -1,16 +1,34 @@
 package com.infinityrefactoring.util.message;
 
+import static java.util.logging.Level.FINEST;
+
 import java.util.Locale;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.logging.Logger;
 
 import javax.el.ELProcessor;
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Default;
 import javax.inject.Inject;
 
+import com.infinityrefactoring.util.text.Expression;
+import com.infinityrefactoring.util.text.ExpressionDefinitions.DollarCurlyBracket;
+import com.infinityrefactoring.util.text.ExpressionException;
+
+@Default
 @RequestScoped
 public class DefaultMessageInterpolator implements MessageInterpolator {
 
 	@Inject
-	private MessageTemplateProvider properties;
+	private Logger logger;
+
+	@Inject
+	private MessageProvider messageProvider;
+
+	@Inject
+	private DollarCurlyBracket dollarCurlyBracket;
 
 	@Inject
 	private ELProcessor elProcessor;
@@ -31,7 +49,30 @@ public class DefaultMessageInterpolator implements MessageInterpolator {
 
 	@Override
 	public String get(String key, Locale locale) {
-		return properties.get(key, locale, expression -> elProcessor.eval(expression.getSubExpression()));
+		if (key == null) {
+			throw new IllegalArgumentException("The key must be not null");
+		}
+
+		Map<String, String> messages = messageProvider.getMessages(locale);
+		String template = messages.get(key);
+
+		if (template == null) {
+			throw new IllegalArgumentException("Not found message for key: " + key);
+		}
+		return interpolate(key, template);
+	}
+
+	private String interpolate(String key, String template) {
+		try {
+			SortedMap<Expression, SortedSet<Integer>> expressions = dollarCurlyBracket.findAll(template, 0);
+			String message = dollarCurlyBracket.interpolate(template, expressions, expression -> elProcessor.eval(expression.getSubExpression()));
+			if (logger.isLoggable(FINEST)) {
+				logger.finest(String.format("\nInteporlated message: {\n    key: \"%s\"\n    template: \"%s\"\n    message: \"%s\"\n}", key, template, message));
+			}
+			return message;
+		} catch (RuntimeException ex) {
+			throw new ExpressionException("Cannot be interpolate the message: " + key, ex);
+		}
 	}
 
 }
---
+++ src/main/java/com/infinityrefactoring/util/message/MessageSource.java
@@ -0,0 +1,17 @@
+package com.infinityrefactoring.util.message;
+
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author Thomás Sousa Silva (ThomasSousa96)
+ */
+public interface MessageSource {
+
+	public static final String MESSAGE_ORDINAL = "message_ordinal";
+
+	public Map<String, String> getMessages(Locale locale);
+
+	public String getName();
+
+}
