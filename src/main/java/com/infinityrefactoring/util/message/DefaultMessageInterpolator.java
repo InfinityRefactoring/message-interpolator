771d8b975ce83f2cfcc0a8241278a02cce81d54a
Thomás Sousa Silva (ThomasSousa96) <thomas.sousa.96@gmail.com>
2018-11-28 23:14:39 -0300

    Add overload to the get method.

--- src/main/java/com/infinityrefactoring/util/message/DefaultMessageInterpolator.java
+++ src/main/java/com/infinityrefactoring/util/message/DefaultMessageInterpolator.java
@@ -20,64 +20,67 @@ import com.infinityrefactoring.util.text.ExpressionException;
 @Default
 @RequestScoped
 public class DefaultMessageInterpolator implements MessageInterpolator {
-
-	@Inject
-	private Logger logger;
-
-	@Inject
-	private MessageProvider messageProvider;
-
-	@Inject
-	private DollarCurlyBracket dollarCurlyBracket;
-
-	@Inject
-	private ELProcessor elProcessor;
-
-	@Inject
-	private List<Locale> locales;
-
-	@Override
-	public MessageInterpolator add(String key, Object value) {
-		elProcessor.defineBean(key, value);
-		return this;
-	}
-
-	@Override
-	public String get(String key) {
-		return get(key, locales);
-	}
-
-	@Override
-	public String get(String key, List<Locale> locales) {
-		if (key == null) {
-			throw new IllegalArgumentException("The key must be not null");
-		}
-
-		String template = null;
-		for (Locale locale : locales) {
-			template = messageProvider.getMessages(locale).get(key);
-			if (template != null) {
-				break;
-			}
-		}
-
-		if (template == null) {
-			throw new IllegalArgumentException("Not found message for key: " + key);
-		}
-		return interpolate(key, template);
-	}
-
-	private String interpolate(String key, String template) {
-		try {
-			SortedMap<Expression, SortedSet<Integer>> expressions = dollarCurlyBracket.findAll(template, 0);
-			String message = dollarCurlyBracket.interpolate(template, expressions, expression -> elProcessor.eval(expression.getSubExpression()));
-			if (logger.isLoggable(FINEST)) {
-				logger.finest(String.format("\nInteporlated message: {\n    key: \"%s\"\n    template: \"%s\"\n    message: \"%s\"\n}", key, template, message));
-			}
-			return message;
-		} catch (RuntimeException ex) {
-			throw new ExpressionException("Cannot be interpolate the message: " + key, ex);
-		}
-	}
-
+    
+    @Inject
+    private Logger logger;
+    
+    @Inject
+    private MessageProvider messageProvider;
+    
+    @Inject
+    private DollarCurlyBracket dollarCurlyBracket;
+    
+    @Inject
+    private ELProcessor elProcessor;
+    
+    @Inject
+    private List<Locale> locales;
+    
+    @Override
+    public MessageInterpolator add(String key, Object value) {
+        elProcessor.defineBean(key, value);
+        return this;
+    }
+    
+    @Override
+    public String get(String key, boolean required) {
+        return get(key, locales, required);
+    }
+    
+    @Override
+    public String get(String key, List<Locale> locales, boolean required) {
+        if (key == null) {
+            throw new IllegalArgumentException("The key must be not null");
+        }
+        
+        String template = null;
+        for (Locale locale : locales) {
+            template = messageProvider.getMessages(locale).get(key);
+            if (template != null) {
+                break;
+            }
+        }
+        
+        if (template == null) {
+            if (required) {
+                throw new IllegalArgumentException("Not found message for key: " + key);
+            }
+            return null;
+        }
+        return interpolate(key, template);
+    }
+    
+    private String interpolate(String key, String template) {
+        try {
+            SortedMap<Expression, SortedSet<Integer>> expressions = dollarCurlyBracket.findAll(template, 0);
+            String message = dollarCurlyBracket.interpolate(template, expressions, expression -> elProcessor.eval(expression.getSubExpression()));
+            if (logger.isLoggable(FINEST)) {
+                logger.finest(String.format("\nInteporlated message: {\n    key: \"%s\"\n    template: \"%s\"\n    message: \"%s\"\n}", key, template, message));
+            }
+            return message;
+        } catch (RuntimeException ex) {
+            throw new ExpressionException("Cannot be interpolate the message: " + key, ex);
+        }
+    }
+    
 }
